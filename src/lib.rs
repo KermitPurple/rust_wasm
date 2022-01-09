d226987e9a07d24e35d84edf763f6a3658db4280
Shane McDonough <shanemcdman@gmail.com>
2022-01-08 20:33:08 -0800

    Add more wasm functions

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,37 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern {
     pub fn alert(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_i32(s: i32);
+
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_many(a: &str, b: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub fn factorial(n: i32) -> i32 {
+    if n < 0 {
+        panic!("n must be greater than 0")
+    } else if n <= 1 {
+        1
+    } else {
+        n * factorial(n - 1)
+    }
+}
+
+#[wasm_bindgen]
+pub fn show_fact(){
+    for i in 0..10 {
+        log_i32(i);
+        log(&format!("{}! = {}", i, factorial(i)));
+    }
 }
